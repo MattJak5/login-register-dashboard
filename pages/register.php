4a1236f92cf07a6c5ade35040ede12592aa3ca78
Matthew Jakeman <146007190+MattJak5@users.noreply.github.com>
2026-05-14 16:30:27 +0100

    new bits

--- pages/register.php
+++ pages/register.php
@@ -8,7 +8,7 @@
 <body>
     <div class="container" id="registration">
         <h1>Register</h1>
-        <form method="post" action="">
+        <form method="post" action="register.php">
             <div class="input_field">
                 <label for="username">Username: </label>
                 <input type="text" name="username" id="username">
@@ -26,7 +26,7 @@
                 <input type="text" name="password_check" id="password_check">
             </div>
 
-            <button type="submit">Submit</button>
+            <button type="submit" name="submit">Submit</button>
         </form>
         <div>
             <p>Already have an account?</p> <a href="login.php">Log in here</a>
@@ -37,5 +37,35 @@
 </html>
 
 <?php 
+    include '../database/db_connection.php';
 
+    if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_check = $_POST['password_check'];
+
+    $securePass = password_hash($password, PASSWORD_DEFAULT);
+    if ($securePass != $password_check) { echo 'Passwords do not match'; exit();}
+
+    $stmt = $dbh->prepare("SELECT email FROM users WHERE email = ?");
+    $stmt->execute([$_POST['email']]);
+    $count = $stmt->rowCount();
+    if ($count > 0) {echo 'email already exists'; exit();}
+
+    $stmt = $dbh->prepare("SELECT username FROM users WHERE username = ?");
+    $stmt->execute([$_POST['usernam']]);
+    $count = $stmt->rowCount();
+    if ($count > 0) {echo 'username already exists', exit();}
+
+    $stmt = $dbh->prepare('INSERT INTO users (username, email, password) VALUES (:username, :email, :password)');
+
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':password', $secure_pass);
+
+    $stmt->execute();
+    header('login.php');
+    exit();
+    }
 ?>
