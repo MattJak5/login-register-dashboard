7507a3fe73251c330c795599da1d63dda2a48e8c
Matthew Jakeman <146007190+MattJak5@users.noreply.github.com>
2026-05-15 15:07:37 +0100

    update to use PDO

--- database/db_connection.php
+++ database/db_connection.php
@@ -5,10 +5,12 @@ $password = "";
 $dbname = "auth";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
+try {
+    $dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error); 
-    }
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
 ?>
