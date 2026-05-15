24404ca3ae116562b97d383b47f52261e4b94b95
Matthew Jakeman <146007190+MattJak5@users.noreply.github.com>
2026-05-15 15:33:10 +0100

    file restructure

--- pages/register.php
+++ pages/register.php
@@ -1,41 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container" id="registration">
-        <h1>Register</h1>
-        <form method="post" action="register.php">
-            <div class="input_field">
-                <label for="username">Username: </label>
-                <input type="text" name="username" id="username">
-            </div>
-            <div class="input_field">
-                <label for="email">Email: </label>
-                <input type="text" name="email" id="email">
-            </div>
-             <div class="input_field">
-                <label for="password">Password: </label>    
-                <input type="password" name="password" id="password">
-            </div>
-                <div class="input_field">
-                <label for="password_check">Re-enter Password: </label>    
-                <input type="password" name="password_check" id="password_check">
-            </div>
-
-            <button type="submit" name="submit">Submit</button>
-        </form>
-        <div>
-            <p>Already have an account?</p> <a href="login.php">Log in here</a>
-        </div>
-    </div>
-    
-</body>
-</html>
-
 <?php 
     include '../database/db_connection.php';
 
@@ -70,3 +32,42 @@
     exit();
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container" id="registration">
+        <h1>Register</h1>
+        <form method="post" action="register.php">
+            <div class="input_field">
+                <label for="username">Username: </label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="input_field">
+                <label for="email">Email: </label>
+                <input type="text" name="email" id="email">
+            </div>
+             <div class="input_field">
+                <label for="password">Password: </label>    
+                <input type="password" name="password" id="password">
+            </div>
+                <div class="input_field">
+                <label for="password_check">Re-enter Password: </label>    
+                <input type="password" name="password_check" id="password_check">
+            </div>
+
+            <button type="submit" name="submit">Submit</button>
+        </form>
+        <div>
+            <p>Already have an account?</p> <a href="login.php">Log in here</a>
+        </div>
+    </div>
+    
+</body>
+</html>
